4a79f4da47735138fb65d9ead6ca3f5a4bcf66e0
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-02-05 16:05:56 -0300

    Update esqueci dos ID

--- app/Model/sqlFiles/Pista1.sql
+++ app/Model/sqlFiles/Pista1.sql
@@ -2,32 +2,32 @@
 * Pista 1
 */
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Indicação', '?', '11:00', '12:59');
+VALUES ('1', '1', 'Indicação', '?', '11:00', '12:59');
 
 -- ZUFFO
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Zuffo', 'EDM', '13:00', '14:59');
+VALUES ('2', '1', 'Zuffo', 'EDM', '13:00', '14:59');
 
 -- Almanac
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Almanac', 'EDM', '15:00', '16:29');
+VALUES ('3', '1', 'Almanac', 'EDM', '15:00', '16:29');
 
 -- Daft Hill
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Daft Hill', 'G-House', '16:30', '17:59');
+VALUES ('4', '1', 'Daft Hill', 'G-House', '16:30', '17:59');
 
 -- Illusionize
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Illusionize', 'Brazilian Bass', '18:00', '19:59');
+VALUES ('5', '1', 'Illusionize', 'Brazilian Bass', '18:00', '19:59');
 
 -- Chemical Surf
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Chemical Surf', 'EDM', '20:00', '21:59');
+VALUES ('6', '1', 'Chemical Surf', 'EDM', '20:00', '21:59');
 
 -- Bruno BE
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Bruno BE', 'Deep House', '22:00', '23:29');
+VALUES ('7', '1', 'Bruno BE', 'Deep House', '22:00', '23:29');
 
 -- Pimp CHIC
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '1', 'Pimp CHIC', 'Tech House', '23:30', '00:59');
+VALUES ('8', '1', 'Pimp CHIC', 'Tech House', '23:30', '00:59');
--- app/Model/sqlFiles/Pista3.sql
+++ app/Model/sqlFiles/Pista3.sql
@@ -2,33 +2,33 @@
 * Pista 3
 */
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'Indicação', '?', '11:00', '12:59');
+VALUES ('16', '3', 'Indicação', '?', '11:00', '12:59');
 
 -- BERVON
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'BERVON', 'Industrial Techno', '13:00', '14:29');
+VALUES ('17', '3', 'BERVON', 'Industrial Techno', '13:00', '14:29');
 
 -- BLANCAH
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'BLANCAh', 'Techno', '14:30', '15:59');
+VALUES ('18', '3', 'BLANCAh', 'Techno', '14:30', '15:59');
 
 -- L_CIO
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'L_CIO', 'Techno', '16:00', '17:59');
+VALUES ('19', '3', 'L_CIO', 'Techno', '16:00', '17:59');
 
 -- ELI IWASA
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'Eli Iwasa', 'Tech House', '18:00', '19:29');
+VALUES ('20', '3', 'Eli Iwasa', 'Tech House', '18:00', '19:29');
 
 -- WEHBBA
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'Wehbba', 'Techno', '19:30', '21:29');
+VALUES ('21', '3', 'Wehbba', 'Techno', '19:30', '21:29');
 
 -- MURPHY
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'Murphy', 'Techno', '21:30', '23:29');
+VALUES ('22', '3', 'Murphy', 'Techno', '21:30', '23:29');
 
 -- RENATO RATIER
 INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
-VALUES (NULL, '3', 'Tech House', '?', '23:30', '00:59');
+VALUES ('23', '3', 'Tech House', '?', '23:30', '00:59');
 
