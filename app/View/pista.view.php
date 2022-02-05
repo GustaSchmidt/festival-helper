33b4107c60cdbcdd749c4c5157150683a7570d35
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-02-05 19:13:29 -0300

    Front pista

---
+++ app/View/pista.view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programação Carnavibe</title>
+
+    <link rel="stylesheet" href="/public_assets/css/master.css">
+    <link rel="stylesheet" href="/public_assets/css/pista.css">
+
+    
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img id="logo-evento" src="/public_assets/imagens/logo.png" alt="">
+        <h1 id="TituloBloco">Bloco 1</h1>     
+        
+        <ul class="pist-list">
+            <div class="container-list">
+                <span class="time"> * </span>
+                <li class="presentation">
+                    <h2>DJ NOME</h2>
+                    <p>Estilo</p>
+                </li>
+            </div>
+            <div class="container-list">
+                <li class="presentation active">
+                    <h2>DJ NOME</h2>
+                    <p>Estilo</p>
+                </li>
+            </div>
+        </ul>
+    
+        <!-- <div class="dev-by">
+            <p>Developer -> <a href="https://github.com/GustaSchmidt" target="_blank" rel="noopener noreferrer">Gustavo Schmidt</a> </p>
+        </div> -->
+    </div>
+</body>
+</html>
