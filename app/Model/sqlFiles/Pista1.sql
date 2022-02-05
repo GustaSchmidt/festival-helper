e77ba7046c36e4c5e4cc8382a391ad76d6879d50
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-02-04 23:13:15 -0300

    Create Pista1.sql

---
+++ app/Model/sqlFiles/Pista1.sql
@@ -0,0 +1,37 @@
+/*
+* Pista 1
+*/
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Indicação', '?', '11:00', '12:59');
+
+-- ZUFFO
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Zuffo', 'EDM', '13:00', '14:59');
+
+-- Almanac
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Almanac', 'EDM', '15:00', '16:29');
+
+-- Daft Hill
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Almanac', 'G-House', '16:30', '17:59');
+
+-- Illusionize
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Illusionize', 'Brazilian Bass', '18:00', '19:59');
+
+-- Chemical Surf
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Chemical Surf', 'EDM', '20:00', '21:59');
+
+-- Chemical Surf
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Chemical Surf', 'EDM', '20:00', '21:59');
+
+-- Bruno BE
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Bruno BE', 'Deep House', '22:00', '23:29');
+
+-- Pimp CHIC
+INSERT INTO `horarios` (`idHorario`, `Pista`, `Artista`, `Estilo`, `HorarioInicio`, `HorarioFim`) 
+VALUES (NULL, '1', 'Pimp CHIC', 'Tech House', '22:00', '23:29');
