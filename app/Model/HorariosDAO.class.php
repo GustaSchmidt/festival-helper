7bb46008f8f744922fb7a14b2551414410b52024
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-02-06 17:26:29 -0300

    Revert "Time no PHP"
    This reverts commit 74af0a325a56d4612624ae30ba4be35dcf1444ce.

--- app/Model/HorariosDAO.class.php
+++ app/Model/HorariosDAO.class.php
@@ -82,7 +82,7 @@ class HorarioDAO extends sqlBasic{
     }
 
     public function queryLinePlay(){
-        $statement = sqlBasic::$conn->prepare("SELECT * FROM `horarios` WHERE HorarioInicio <= ".date("H:i")." AND HorarioFim >= ".date("H:i"));
+        $statement = sqlBasic::$conn->prepare("SELECT * FROM `horarios` WHERE HorarioInicio <= CURRENT_TIME AND HorarioFim >= CURRENT_TIME");
         
         $statement->execute();
         $result = $statement->fetchAll();
